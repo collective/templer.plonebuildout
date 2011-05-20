ca3340238744b27b9ae736849b0337ab861c6737
davidray <davidray@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-05-20 18:53:41 +0000

    initial commit
    git-svn-id: https://svn.plone.org/svn/collective/templer.plonebuildout/trunk@239731 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+tests_require=[
+    'Cheetah', 
+    'PasteScript',
+    'templer.core',
+],
+
+setup(name='templer.plonebuildout',
+      version=version,
+      description="",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['templer'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      tests_require=tests_require,
+      test_suite='templer.plone.tests.test_all.test_suite',
+      extras_require=dict(test=tests_require),      
+      entry_points="""
+      # -*- Entry points: -*-
+      [paste.paster_create_template]
+      plone3_buildout = templer.plonebuildout:Plone3Buildout
+      plone4_buildout = templer.plonebuildout:Plone4Buildout
+      """,
+      )
